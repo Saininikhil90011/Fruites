71305ccade9d3f22b44a85ae75ed8c36fb8b2806
Saininikhil90011 <saininikhil90011@gmail.com>
2025-01-31 11:39:48 +0530

    add

--- fruitablesbackend/conn.js
+++ fruitablesbackend/conn.js
@@ -1,36 +1,41 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(process.env.URL);
-mongoose.connection.on('error', err => {
-    console.log(err);
+mongoose.connection.on("error", (err) => {
+  console.log(err);
 });
-mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on("connected", () => console.log("connected"));
 
 //create collection
 const { Schema } = mongoose;
 const contactSchema = new Schema({
-    name: String, // String is shorthand for {type: String}
-    email: String,
-    message: String,
+  name: String, // String is shorthand for {type: String}
+  email: String,
+  message: String,
 });
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 const productSchema = new Schema({
-    type: String,
-    fName: String,
-    fTitle: String,
-    fPrice: Number,
-    fImage: String,
+  type: String,
+  fName: String,
+  fTitle: String,
+  fPrice: Number,
+  fImage: String,
 });
-const userSchema=new Schema({
-    name: String,
-    email:String,
-    phone:String,
-    password:String
-})
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  phone: String,
+  password: String,
+});
+const dataSchema = new Schema({
+  user: String,
+  message: String,
+});
 
-const Product = mongoose.model('Product', productSchema);
-const User = mongoose.model('User', userSchema);
+const Product = mongoose.model("Product", productSchema);
+const Data = mongoose.model("Data", dataSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = { Contact, Product,User }
+module.exports = { Contact, Product, User, Data };
